171d2c0ecc2b289948c16ebe71609606870f2666
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-12-05 22:09:17 +0100

    Publish 0.10.0-alpha.1

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,3 @@
-### 0.10.0
+### 0.10.0-alpha.1
 
 * Upgrade to Fable 0.7.0
--- build.js
+++ build.js
@@ -3,23 +3,29 @@ var fs = require("fs-extra");
 var fable = require("fable-compiler");
 // var fable = require("../Fable/build/fable");
 
+var version;
+
 var targets = {
     all() {
         return fable.promisify(fs.remove, "npm")
-            .then(_ => fable.promisify(fs.remove, "build"))
             .then(_ => fable.compile({projFile: "src/Fable.Arch"}))
             .then(_ => fable.compile({projFile: "src/Fable.Arch", target: "umd"}))
             .then(_ => fable.promisify(fs.copy, "package.json", "npm/package.json"))
             .then(_ => fable.promisify(fs.copy, "README.md", "npm/README.md"))
             .then(_ => fable.promisify(fs.readFile, "RELEASE_NOTES.md"))
             .then(line => {
-                var version = /\d[^\s]*/.exec(line)[0];
+                version = /\d[^\s]*/.exec(line)[0];
                 return fable.runCommand("npm", "npm version " + version);
             });
     },
     publish() {
         return this.all()
-            .then(_ => fable.runCommand("npm", "npm publish"))
+            .then(_ => {
+                var command = version && version.indexOf("alpha") > -1
+                    ? "npm publish --tag next"
+                    : "npm publish";
+                fable.runCommand("npm", command)
+            })
     }
 }
 
